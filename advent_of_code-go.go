191842041cb5b58d47e2b431540ba2493f08fb9d
Mason Fisher <masonfisherb@gmail.com>
2023-08-12 16:37:55 -0500

    day 2 part 1

--- advent_of_code-go.go
+++ advent_of_code-go.go
@@ -9,7 +9,8 @@ import (
 )
 
 func main(){
-
+    fmt.Println("Day 1")
+    //part 1 & 2 
     day_1_file, _ := os.Open("./prod_1")
     scanner := bufio.NewScanner(day_1_file)
     list := []int{}
@@ -25,7 +26,6 @@ func main(){
             b = b + a
         }
     }
-    
     slices.Sort(list)
     slices.Reverse[[]int](list)
     fmt.Print("part 1: ")
@@ -33,6 +33,32 @@ func main(){
     fmt.Print("part 2: ")
     fmt.Println(list[0] + list[1] + list[2])
 
-    //day_2_file, _ := os.Open("./prod_2")
+    fmt.Println("Day 2")
+    //part 1 
+    day_2_file, _ := os.Open("./prod_2")
+    scanner = bufio.NewScanner(day_2_file)
+    sum := 0
+    for scanner.Scan(){
+        line := scanner.Text()
+        strings.Trim(line, "\n ")
+        var opponent, me rune 
+        fmt.Sscanf(line, "%c %c", &opponent, &me)
+        sum = sum + get_score(opponent, me)
+    }
+    fmt.Print("part 1: ")
+    fmt.Println(sum)
 
+}
+
+func get_score(o, m rune) int {
+    var opp, me = int(o), int(m)
+    opp = opp - 65
+    me = me - 88
+    score := 0
+    if (opp + 1) % 3 == me {
+        score = 6
+    } else if opp == me {
+        score = 3
+    }
+    return me + score + 1
 }
